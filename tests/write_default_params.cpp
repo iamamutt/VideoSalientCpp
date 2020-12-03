2ec8169fe1bb71ac73d25db24af817461341f334
Joseph M. Burling <josephburling@gmail.com>
2020-12-02 20:02:30 -0600

    add writing default parameters binary

---
+++ tests/write_default_params.cpp
@@ -0,0 +1,20 @@
+/**
+    write the parameter defaults to file
+    @file write_default_params.cpp
+    @author Joseph M. Burling
+*/
+
+#include "command_line.h"
+#include "parameters.h"
+#include "tools.h"
+
+int
+main(int argc, char **argv)
+{
+  auto [opts, parser] = parse_command_line_args(argc, argv);
+  if (opts.out_dir.empty()) opts.out_dir = ".";
+  auto pars_file = normalize_path(opts.out_dir, "saliency/share/parameters.yml");
+  std::cout << "Writing default parameters to: " << pars_file << std::endl;
+  params::parameter_defaults(pars_file);
+  exit(0);
+}
